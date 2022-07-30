a829d04af7292151128ab070d039ddcd62de5286
chriskim0708 <last1224@gmail.com>
2022-07-30 22:40:16 +0900

    feat: form phone, credit validation 추가 및 valid check 기능 추가

--- src/composables/useForm.vue
+++ src/composables/useForm.vue
@@ -3,8 +3,9 @@ import { reactive } from "vue";
 
 enum Valid {
   REQUIRED = "REQUIRED",
-  PHONE = "PHONE",
+  PHONE_NUMBER = "PHONE_NUMBER",
   EMAIL = "EMAIL",
+  CREDIT_CARD = "CREDIT_CARD",
 }
 export type TValid = `${Valid}`;
 
@@ -16,19 +17,65 @@ const useForm = <T extends Object>(initialFormValue: T) => {
       /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
     return regex.test(value);
   };
+  const isValidPhoneNumber = (value: string): boolean => {
+    if (!value) return false;
+    const regex = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
+    return regex.test(value);
+  };
   const isValidRequired = (value: string): boolean => {
     return !!value;
   };
+  const isValidCreditCardNumber = (value: string): boolean => {
+    if (!value) return false;
+    const regex = /([0-9]{4})-?([0-9]{4})-?([0-9]{4})-?([0-9]{4})/;
+    const regTest = regex.test(value);
+    console.log("card regTest", regTest);
+    if (!regTest) return false;
+    const reverse = value
+      .replace(/[^0-9]/g, "")
+      .split("")
+      .reverse();
+    const sumOdd = reverse.reduce((a: number, b: string, idx: number) => {
+      if ((idx + 1) % 2 !== 0) {
+        return Number(b) + a;
+      }
+      return a;
+    }, 0);
+    const sumEven = reverse.reduce((a: number, b: string, idx: number) => {
+      if ((idx + 1) % 2 === 0) {
+        const sumNumber =
+          Number(b) * 2 >= 10
+            ? String(Number(b) * 2)
+                .split("")
+                .reduce((c: number, d: string) => c + Number(d), 0)
+            : Number(b) * 2;
+        return sumNumber + a;
+      }
+      return a;
+    }, 0);
+    console.log("sum", sumOdd + sumEven);
+    return (sumOdd + sumEven) % 10 === 0;
+  };
   const matchValidFn: { [key in TValid]: Function } = {
     REQUIRED: isValidRequired,
-    PHONE: isValidRequired,
+    PHONE_NUMBER: isValidPhoneNumber,
     EMAIL: isValidEmail,
+    CREDIT_CARD: isValidCreditCardNumber,
   };
-  const isValidCheck = (error: TValid[] = []) => {
+  const isValidCheck = ({
+    value,
+    error = [],
+  }: {
+    value: string;
+    error: TValid[];
+  }) => {
     if (error.length === 0) return true;
+    console.log("isValid", error);
     return error
-      .map((errorName: TValid) => matchValidFn[errorName]())
-      .every((valid) => valid === true);
+      .map((errorName: TValid) =>
+        matchValidFn[errorName](value.replace(/ /g, ""))
+      )
+      .every((valid: boolean) => valid === true);
   };
   return { formValue, isValidCheck };
 };
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -3,28 +3,79 @@
     <h1>사용자 정보 입력</h1>
     <div class="form-box">
       <form>
-        <div>
-          <label>이름</label>
-          <input type="text" v-model="formValue.name" placeholder="이름" />
-        </div>
-        <div>
-          <label>번호</label>
-          <input
-            type="text"
-            v-model="formValue.phoneNumber"
-            placeholder="휴대폰 번호"
-          />
-        </div>
-        <div>
-          <label>이메일</label>
-          <input type="text" v-model="formValue.email" placeholder="이메일" />
-        </div>
+        <template v-if="step === 0">
+          <div>
+            <label>이름</label>
+            <input
+              type="text"
+              v-model="formValue.name.value"
+              placeholder="이름"
+            />
+          </div>
+          <div>
+            <label>번호</label>
+            <input
+              type="text"
+              v-model="formValue.phoneNumber.value"
+              placeholder="휴대폰 번호"
+            />
+          </div>
+          <div>
+            <label>이메일</label>
+            <input
+              type="text"
+              v-model="formValue.email.value"
+              placeholder="이메일"
+            />
+          </div>
+        </template>
+        <template v-else-if="step === 1">
+          <div>
+            <label>우편번호</label>
+            <input
+              type="text"
+              v-model="formValue.zipCode.value"
+              placeholder="우편번호"
+            />
+            <button type="button" @click="showPostalPopup">
+              우편번호 찾기
+            </button>
+          </div>
+          <div>
+            <label>주소</label>
+            <input
+              type="text"
+              v-model="formValue.address.value"
+              placeholder="주소"
+            />
+          </div>
+          <div>
+            <label>상세주소</label>
+            <input
+              type="text"
+              v-model="formValue.detailAddress.value"
+              placeholder="상세주소"
+            />
+          </div>
+        </template>
+        <template v-else-if="step === 2">
+          <div>
+            <label>신용카드</label>
+            <input
+              type="text"
+              v-model="formValue.creditCardNumber.value"
+              placeholder="신용카드"
+            />
+          </div>
+        </template>
       </form>
     </div>
     <div>
-      <button :disabled="step === 0">이전</button>
+      <button :disabled="step === 0" @click="goPrevStep">이전</button>
       <button @click="goNextStep(step)">다음</button>
-      {{ step }}
+    </div>
+    <div v-if="print">
+      {{ JSON.stringify(print) }}
     </div>
   </div>
 </template>
@@ -34,7 +85,7 @@ import { defineComponent, ref } from "vue";
 import useForm, { TValid } from "../composables/useForm.vue";
 
 interface FormControl {
-  value: string | number;
+  value: string;
   error: TValid[];
 }
 
@@ -49,10 +100,22 @@ type FormName =
 
 type InitialFormValue = { [key in FormName]: FormControl };
 
+interface PrintData {
+  name: string;
+  address: {
+    addr1: string;
+    addr2: string;
+    postal: string;
+  };
+  email: string;
+  creditNumber: string;
+}
+
 export default defineComponent({
   name: "MultiForm",
   setup() {
     const step = ref(0);
+    const print = ref<null | PrintData>(null);
     const { formValue, isValidCheck } = useForm<InitialFormValue>({
       name: {
         value: "",
@@ -60,7 +123,7 @@ export default defineComponent({
       },
       phoneNumber: {
         value: "",
-        error: ["REQUIRED", "PHONE"],
+        error: ["REQUIRED", "PHONE_NUMBER"],
       },
       email: {
         value: "",
@@ -68,29 +131,108 @@ export default defineComponent({
       },
       zipCode: {
         value: "",
-        error: [],
+        error: ["REQUIRED"],
       },
       address: {
         value: "",
-        error: [],
+        error: ["REQUIRED"],
       },
       detailAddress: {
         value: "",
-        error: [],
+        error: ["REQUIRED"],
       },
       creditCardNumber: {
         value: "",
-        error: [],
+        error: ["REQUIRED", "CREDIT_CARD"],
       },
     });
+    const goPrevStep = () => {
+      if (step.value > 0) {
+        step.value--;
+      }
+    };
     const goNextStep = () => {
-      isValidCheck(["REQUIRED"]);
+      switch (step.value) {
+        case 0: {
+          const valid = [
+            isValidCheck(formValue.name),
+            isValidCheck(formValue.phoneNumber),
+            isValidCheck(formValue.email),
+          ].every((valid) => valid === true);
+          console.log(isValidCheck(formValue.email));
+          valid && step.value++;
+          break;
+        }
+        case 1: {
+          const valid = [
+            isValidCheck(formValue.zipCode),
+            isValidCheck(formValue.address),
+            isValidCheck(formValue.detailAddress),
+          ].every((valid) => valid === true);
+          valid && step.value++;
+          break;
+        }
+        case 2: {
+          const valid = isValidCheck(formValue.creditCardNumber);
+          valid && printInputData();
+          break;
+        }
+        default: {
+          return false;
+        }
+      }
+    };
+    const showPostalPopup = () => {
+      new window.daum.Postcode({
+        oncomplete: (data: any) => {
+          let fullRoadAddr = data.roadAddress;
+          let extraRoadAddr = "";
+
+          if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+            extraRoadAddr += data.bname;
+          }
+          if (data.buildingName !== "" && data.apartment === "Y") {
+            extraRoadAddr +=
+              extraRoadAddr !== ""
+                ? ", " + data.buildingName
+                : data.buildingName;
+          }
+          if (extraRoadAddr !== "") {
+            extraRoadAddr = " (" + extraRoadAddr + ")";
+          }
+
+          if (fullRoadAddr !== "") {
+            fullRoadAddr += extraRoadAddr;
+          }
+
+          formValue.zipCode.value = data.zonecode;
+          formValue.address.value = fullRoadAddr;
+        },
+      }).open();
+    };
+    const printInputData = () => {
+      print.value = {
+        name: formValue.name.value,
+        address: {
+          addr1: formValue.address.value,
+          addr2: formValue.detailAddress.value,
+          postal: String(formValue.zipCode),
+        },
+        email: formValue.email.value,
+        creditNumber: formValue.creditCardNumber.value.replace(
+          /^(\d{4})(\d{4})(\d{4})(\d{4})$/,
+          `$1-$2-$3-$4`
+        ),
+      };
     };
 
     return {
       step,
+      print,
       formValue,
+      goPrevStep,
       goNextStep,
+      showPostalPopup,
     };
   },
 });
